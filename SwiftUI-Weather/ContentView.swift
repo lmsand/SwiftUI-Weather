dcc0ec383a1c36b70f87f6a782c47996efadbda1
lmsand <lmsand99@icloud.com>
2023-12-12 02:03:26 -0600

    project setup

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium))
+                    .foregroundStyle(.white)
+                    .padding()
+                
+                VStack(spacing: 8) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 180, height: 180)
+                    
+                    Text("78°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(.white)
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
